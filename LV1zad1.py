db66085179ee3733736eac6bbf241fd8959c7c2e
antonijasula <85222568+antonijasula@users.noreply.github.com>
2025-03-11 20:51:37 +0100

    Delete LV1zad1.py

--- LV1zad1.py
+++
@@ -1,8 +0,0 @@
-def total_euro(radni_sati,eura_h):
-    ukupno=radni_sati*eura_h
-    return ukupno
-
-radni_sati=int(input("Radni sati: "))
-eura_h=float(input("eura/h: "))
-print("Ukupno iznos: ",total_euro(radni_sati,eura_h))
-
