406da45293d802f15eff5ec6aafb2d812bad1e1b
antonijasula <85222568+antonijasula@users.noreply.github.com>
2025-03-11 13:35:24 +0100

    Update LV1zad1.py

--- LV1zad1.py
+++ LV1zad1.py
@@ -1,1 +1,8 @@
+def total_euro(radni_sati,eura_h):
+    ukupno=radni_sati*eura_h
+    return ukupno
+
+radni_sati=int(input("Radni sati: "))
+eura_h=float(input("eura/h: "))
+print("Ukupno iznos: ",total_euro(radni_sati,eura_h))
 
