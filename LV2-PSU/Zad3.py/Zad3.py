9d8baf0eb4d1ceb94b97bb601c1c82b11548ead9
antonijasula <85222568+antonijasula@users.noreply.github.com>
2025-03-21 18:53:54 +0100

    Update Zad3.py

---
+++ LV2-PSU/Zad3.py/Zad3.py
@@ -0,0 +1,25 @@
+img = plt.imread("tiger.png")
+img = img[:,:,0].copy()
+
+print(img.shape)
+
+imgFlipped = np.fliplr(img)
+imgRotated = np.rot90(img,3)
+
+imgResolution = img[::5,::5]
+imgBrightness = img+0.8
+imgBrightness[imgBrightness>1] = 1
+
+visina, sirina = img.shape
+
+print(sirina*0.25, sirina*0.5)
+
+novaSlika = img
+
+novaSlika[:,sirina//2:] = 0
+novaSlika[:,:sirina//4] = 0
+   
+
+plt.figure()
+plt.imshow(novaSlika, cmap="gray")
+plt.show()
